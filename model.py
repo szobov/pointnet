b2e2788943c439198da392bb8f89c9c18758ac73
Sergei Zobov <sergey@szobov.ru>
2022-07-01 10:02:49 +0200

    Add PointNet model

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class GlobalMaxPooling(nn.Module):
 
 class TNet(nn.Module):
 
-    def __init__(self, feature_transfom_net: bool, train: bool = True):
+    def __init__(self, feature_transfom_net: bool):
         super().__init__()
         # output 3x3 matrix (initialized as identity matrix)
         # shared MLP(64, 128, 1024) on each point
@@ -48,7 +48,6 @@ class TNet(nn.Module):
             nn.Linear(512, 256),
             nn.ReLU(),
             nn.BatchNorm1d(256),
-
         ]
 
         # The didn't clearly mention it in paper
@@ -62,21 +61,94 @@ class TNet(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         assert x.shape[1] == 3  # batch_size, 3, number_of_points
         result = self._model(x)
-        expected_output_dim = 9
-        matrix_shape = (3, 3)
+        expected_dim = 9
+        output_matrix_shape = (3, 3)
         if self._feature_transform_net:
-            expected_output_dim = 4096
-            matrix_shape = (64, 64)
+            expected_dim = 4096
+            output_matrix_shape = (64, 64)
 
-        assert result.shape[-1] == expected_output_dim, f"{result.shape[-1]} != {expected_output_dim}"
+        assert result.shape[-1] == expected_dim, f"{result.shape[-1]} != {expected_dim}"
 
         initial_matrix = torch.nn.init.eye_(
-            torch.empty(size=matrix_shape)).reshape(1, expected_output_dim).repeat(x.shape[0], 1)
+            torch.empty(size=output_matrix_shape)).reshape(1, expected_dim).repeat(x.shape[0], 1)
 
         assert initial_matrix.shape == result.shape, f"{initial_matrix.shape} != {result.shape}"
-        new_shape = result.shape[:-1] + matrix_shape
+        new_shape = result.shape[:-1] + output_matrix_shape
         return (initial_matrix + result).reshape(new_shape)
 
 
 class PointNet(nn.Module):
-    ...
+
+    def __init__(self, feature_transfom_net: bool, number_of_classes: int,
+                 is_train: bool = True):
+        super().__init__()
+        self._is_train = is_train
+        self._number_of_classes = number_of_classes
+        self._transform_net = TNet(feature_transfom_net=False)
+        self._mlps_1 = nn.Sequential(*[
+            nn.Conv1d(3, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+
+            nn.Conv1d(64, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+        ])
+        self._feature_transform_net = TNet(feature_transfom_net=True)
+
+        self._mlps_2 = nn.Sequential(*[
+            nn.Conv1d(64, 64, 1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+
+            nn.Conv1d(64, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+
+            nn.Conv1d(128, 1024, 1),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+        ])
+        self._global_max_pool = GlobalMaxPooling()  # (batch_size, 1024)
+        self._mlps_3 = nn.Sequential(*[
+            nn.Conv1d(1024, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Conv1d(512, 256, 1),
+            nn.BatchNorm1d(256),
+            nn.ReLU()
+        ])
+
+        self._dropout = nn.Dropout(p=0.7)
+
+        self._mlps_4 = nn.Sequential(*[
+            nn.Conv1d(256, number_of_classes, 1),
+            nn.BatchNorm1d(number_of_classes),
+            nn.ReLU()
+        ])
+
+    def forward(self, points: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        batch_size = points.shape[0]
+        assert points.shape[1] == 3, points.shape
+        transform_matrix = self._transform_net(points)
+        assert transform_matrix.shape == (batch_size, 3, 3), transform_matrix.shape
+        transformed_points = torch.bmm(transform_matrix, points)
+        features = self._mlps_1(transformed_points)
+        assert features.shape[1] == 64
+
+        feature_transform_matrix = self._feature_transform_net(features)
+        assert feature_transform_matrix.shape == (batch_size, 64, 64), feature_transform_matrix.shape
+        transformed_features = torch.bmm(feature_transform_matrix, features)
+        transformed_features = self._mlps_2(transformed_features)
+
+        assert transformed_features.shape[1] == 1024, transformed_features.shape
+        global_features = self._global_max_pool(transformed_features)
+        assert global_features.shape == (batch_size, 1024), global_features.shape
+        global_features = self._mlps_3(global_features)
+        if self._is_train:
+            global_features = self._dropout(global_features)
+        scores = self._mlps_4(global_features)
+        assert scores.shape == (batch_size, self._number_of_classes), scores.shape
+
+        return scores, feature_transform_matrix  # to calculate regularization
