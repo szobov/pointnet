64a774009f9e99bdf5822ecc011484be1320136e
Sergei Zobov <sergey@szobov.ru>
2022-07-10 18:30:11 +0200

    Add augmentation of the ModelNet dataset

--- dataset.py
+++ dataset.py
@@ -13,8 +13,9 @@ from torch.utils.data import Dataset
 class ModelNet(Dataset):
 
     def __init__(self, dataset_dir: pathlib.Path, train: bool = True,
-                 points_number: int = 1000):
+                 points_number: int = 1024):
         super().__init__()
+        self._train = train
         self._points_number = points_number
         self._classes: dict[str, int] = {
             cls: idx for idx, cls in enumerate(sorted(map(
@@ -49,9 +50,54 @@ class ModelNet(Dataset):
         file_path = self.files[index]
         cls = self.classes[file_path.parents[1].stem]
         mesh = trimesh.load(str(file_path))
-        sampled_points = mesh.sample(count=self._points_number)
-        points = torch.from_numpy(sampled_points.astype(np.float32).T)
+        sampled_points = mesh.sample(count=self._points_number).astype(np.float32)
+        if self._train:
+            sampled_points = self._augment_points(sampled_points)
+
+        points = torch.from_numpy(sampled_points.T)
         return points, torch.from_numpy(np.array([cls]).astype(np.int64))
+
+    def _augment_points(self, points: np.ndarray) -> np.ndarray:
+        return self._jitter_points(
+                self._rotate_around_z_axis(
+                    self._normalize_to_unit_sphere(
+                        self._shuffle_points(points)
+                    )
+                )
+            )
+
+    def _shuffle_points(self, points: np.ndarray) -> np.ndarray:
+        return points[np.random.choice(len(points), len(points), replace=True), :]
+
+    def _normalize_to_unit_sphere(self, points: np.ndarray) -> np.ndarray:
+        # I didn't get what they meant by "normalize into a unit sphere",
+        # but this normalization should feat this definition
+        points /= np.max(np.linalg.norm(points - np.mean(points), axis=-1))
+        assert len(points) == self._points_number
+        return points
+
+    def _rotate_around_z_axis(self, points: np.ndarray) -> np.ndarray:
+        # In the paper they mentioned "along up-axis", but the problem that
+        # "up-axis" depends on how you chose the order. I assume they
+        # used right-hand rule and "up-axis" will be "z-axis". I also
+        # assumed that "along" in this case means "around".
+        angle = 2 * np.pi * np.random.uniform()
+        cos = np.cos(angle)
+        sin = np.sin(angle)
+        # https://en.wikipedia.org/wiki/Rotation_matrix#Basic_rotations
+        rotation_matrix = np.array([
+            [cos, -sin, 0],
+            [sin, cos, 0],
+            [0, 0, 1]
+        ])
+        rotated_points = np.dot(rotation_matrix, points.T).T
+        assert points.shape == rotated_points.shape
+        return rotated_points
+
+    def _jitter_points(self, points: np.ndarray) -> np.ndarray:
+        jitter = 0.02 * np.random.randn(*points.shape)
+        assert jitter.shape == points.shape
+        return points + jitter
 
     def __len__(self) -> int:
         return(len(self.files))
--- visualization_utils.py
+++ visualization_utils.py
@@ -13,31 +13,44 @@ def main(
         dataset_dir: pathlib.Path = pathlib.Path("/home/szobov/dev/learning/pointnet/dataset/ModelNet40"),
         output_file: pathlib.Path = pathlib.Path("/tmp/modelnet.html")
 ):
-    dataset = ModelNet(dataset_dir)
+    dataset = ModelNet(dataset_dir, train=False)
     scene = sp.Scene()
     mesh_canvas = scene.create_canvas_3d(
         "mesh", width=400, height=400,
         shading=sp.Shading(bg_color=sp.Colors.White))
     points_canvas = scene.create_canvas_3d(
-        "points ", width=400, height=400,
+        "points", width=400, height=400,
         shading=sp.Shading(bg_color=sp.Colors.White))
-    scene.link_canvas_events(mesh_canvas, points_canvas)
+    augmented_points_canvas = scene.create_canvas_3d(
+        "augmented points", width=400, height=400,
+        shading=sp.Shading(bg_color=sp.Colors.White))
+    scene.link_canvas_events(mesh_canvas, points_canvas, augmented_points_canvas)
     for indx in random.choices(range(len(dataset)), k=10):
         item = dataset[indx]
         text = dataset.get_class_description(int(item[1][0]))
         mesh_frame = mesh_canvas.create_frame()
         points_frame = points_canvas.create_frame()
+        augmented_points_frame = augmented_points_canvas.create_frame()
         mesh = scene.create_mesh()
         for face in trimesh.load(dataset.files[indx]).triangles:
             mesh.add_triangle(color=sp.Colors.Red,
                               p0=face[0], p1=face[1], p2=face[2])
         mesh.apply_transform(sp.Transforms.Scale(0.01))
         mesh_frame.add_mesh(mesh)
+
         points = scene.create_mesh()
         points.add_sphere(color=sp.Colors.Green)
         points.apply_transform(sp.Transforms.Scale(0.01))
-        points.enable_instancing(item[0] / 100)
+        points.enable_instancing(dataset._normalize_to_unit_sphere(
+            item[0].numpy().T))
         points_frame.add_mesh(points)
+
+        augmented_points = scene.create_mesh()
+        augmented_points.add_sphere(color=sp.Colors.Blue)
+        augmented_points.apply_transform(sp.Transforms.Scale(0.01))
+        augmented_points.enable_instancing(dataset._augment_points(item[0].numpy().T))
+        augmented_points_frame.add_mesh(augmented_points)
+
         label = scene.create_label(
             text=text, color=sp.Colors.Black,
             size_in_pixels=80, offset_distance=0.6, camera_space=True)
